f2ce57d0f08f707a5e1ef9b3a0f6c4a1b26421ab
Ismail KPL <67828542+ismailKPL@users.noreply.github.com>
2023-06-21 20:47:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// const imgGym = document.querySelector(".imggym")
+
+// window.addEventListener("scroll", () => {
+//     console.log(window.scrollY);
+  
+//     if (window.scrollY > 200 && window.scrollY < 1300) {
+//         imgGym.style.width = "50%";
+//       } else {
+//         imgGym.style.width = "70%";
+//       }
+//   });
